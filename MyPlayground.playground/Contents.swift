aa0413057b5c80d201fd1198ebbdeac61e9f7aa1
Thiago Bertolino <thiagopbertolino@gmail.com>
2022-06-18 16:29:21 -0300

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var str = "Hello, World!"
+str = "Test"
+
+var number = 23
+var population = 8_000
+
+var quebrandoLinhas = """
+Teste de
+quebra de linha
+"""
+
+var quebrandoLinhas2 = """
+Teste de \
+quebra de linha
+"""
